13f605efa9360f50edd27c3fcc72bdf145873473
TahaOuarrak <tahaouarrak@gmail.com>
2021-11-07 06:57:00 +0100

    Add api fetch

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,20 +1,33 @@
 import {ListItem, ListItemText} from "@mui/material";
-import {interval, take, tap} from "rxjs";
+import {concatAll, concatMap, delay, interval, of, take, tap} from "rxjs";
 import {useEffect, useState} from "react";
-import {map} from "rxjs/operators";
+import {map, mergeMap} from "rxjs/operators";
+import {ajax} from "rxjs/ajax";
 
 const source = ['user 1', 'user 2', 'user 3'];
-const names$ = interval(1000).pipe(
-  take(source.length),
-  tap(v => console.log(v)),
-  map(i => source.slice(0, i + 1))
-);
+
+// const names$ = of(...source);
+
+/*const names$ = of(...source).pipe(
+  concatMap(x => of(x).pipe(delay(1000))),
+);*/
+
+const api = `https://randomuser.me/api/?results=5&seed=rx-react&nat=us&inc=name&noinfo`;
+const names$ = ajax.getJSON(api)
+  .pipe(
+    map(({ results }) => results.map(user => `${user.name.first} ${user.name.last}`)),
+    concatAll(),
+  );
 
 export default function UserList() {
   const [names, setNames] = useState([]);
   
   useEffect(() => {
-    const subscription = names$.subscribe(setNames);
+    const subscription = names$.subscribe(val => setNames(prevNames => [
+      ...prevNames,
+      val
+    ]));
+    
     return () => subscription.unsubscribe();
   }, []);
   
